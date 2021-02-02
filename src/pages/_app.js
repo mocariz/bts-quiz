ae11bb3c1d33076f3b51b6a797a2076e58571aaa
Monica Mota Ocariz <monica.ocariz@ingaia.com.br>
2021-02-01 22:27:15 -0300

    Adicionado manifest e favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link rel="shortcut icon" href="/img/icon.png" />
+        <link rel="apple-touch-icon" href="/img/icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta
+          name="description"
+          content="Quiz BTS é um projeto construido durante a Imersão React edição NextJS da Alura! Teste seus conhecimentos sobre o BTS!"
+        />
+
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
